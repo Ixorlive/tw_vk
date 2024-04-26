a46a444bee77613bae96426ccafe0ddbd7a8f290
ixorlive <igorshakirov43@gmail.com>
2024-04-27 01:37:56 +0300

    temp solution

--- backend/services/auth/internal/app/app.go
+++ backend/services/auth/internal/app/app.go
@@ -1,6 +1,25 @@
 package app
 
-import "github.com/Ixorlive/tw_vk/backend/services/auth/config"
+import (
+	"fmt"
+
+	"github.com/Ixorlive/tw_vk/backend/services/auth/config"
+	"github.com/Ixorlive/tw_vk/backend/services/auth/internal/controller/http"
+	"github.com/Ixorlive/tw_vk/backend/services/auth/internal/usecase"
+	"github.com/Ixorlive/tw_vk/backend/services/auth/internal/usecase/repo"
+	"github.com/Ixorlive/tw_vk/backend/services/auth/pkg/postgres"
+)
 
 func Run(cfg *config.Config) {
+	pg, err := postgres.New(cfg.PG.URL)
+	if err != nil {
+		fmt.Printf("Error to try connect to postgres: %s", err.Error())
+		return
+	}
+	defer pg.Close()
+
+	authService := usecase.NewAuthService(repo.New(pg))
+
+	router := http.NewRouter(authService)
+	router.Run(cfg.HTTP.Port)
 }
