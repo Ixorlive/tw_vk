d096f34db362a0acf4570b85383617d2624eaa61
ixorlive <igorshakirov43@gmail.com>
2024-04-27 20:43:08 +0300

    impl app start point

--- backend/services/notes/internal/app/app.go
+++ backend/services/notes/internal/app/app.go
@@ -6,19 +6,18 @@ import (
 	"github.com/Ixorlive/tw_vk/backend/services/notes/config"
 	"github.com/Ixorlive/tw_vk/backend/services/notes/internal/controller/http"
 	"github.com/Ixorlive/tw_vk/backend/services/notes/internal/usecase"
+	"github.com/Ixorlive/tw_vk/backend/services/notes/internal/usecase/repo"
 	"github.com/Ixorlive/tw_vk/backend/services/notes/pkg/postgres"
 )
 
 func Run(cfg *config.Config) {
-	// pg, err := postgres.New(cfg.PG.URL)
-	// if err != nil {
-	// 	fmt.Printf("Error to try connect to postgres: %s", err.Error())
-	// 	return
-	// }
-	// defer pg.Close()
-	//
-	// authService := usecase.NewAuthService(repo.New(pg))
-	//
-	// router := http.NewRouter(authService)
-	// router.Run(cfg.HTTP.Port)
+	pg, err := postgres.New(cfg.PG.URL)
+	if err != nil {
+		fmt.Printf("Error to try connect to postgres: %s", err.Error())
+		return
+	}
+	defer pg.Close()
+	noteService := usecase.NewNoteService(repo.NewPGNoteRepo(pg))
+	router := http.NewRouter(noteService)
+	router.Run(cfg.HTTP.Port)
 }
