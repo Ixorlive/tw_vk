5c3c2b7d38f78f0a40cad8bdbc23bfb5d016454b
ixorlive <igorshakirov43@gmail.com>
2024-04-28 00:38:47 +0300

    add swagger to router

--- backend/services/notes/internal/controller/http/router.go
+++ backend/services/notes/internal/controller/http/router.go
@@ -3,10 +3,19 @@ package http
 import (
 	"github.com/Ixorlive/tw_vk/backend/services/notes/internal/usecase"
 	"github.com/gin-gonic/gin"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	_ "github.com/Ixorlive/tw_vk/backend/services/notes/docs"
 )
 
 func NewRouter(noteService usecase.NoteService) *gin.Engine {
 	router := gin.Default()
+	// swagger
+	swaggerHandler := ginSwagger.DisablingWrapHandler(swaggerFiles.Handler, "DISABLE_SWAGGER_HTTP_HANDLER")
+	router.GET("/swagger/*any", swaggerHandler)
+
 	noteController := NewNoteController(noteService)
 	noteController.RegisterRoutes(router)
 	return router
